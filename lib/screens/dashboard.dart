f9e5c9c847a7cc7cece2e10d470d0709750d6730
Shalini241 <shalinidtu@gmail.com>
2020-11-06 19:56:42 +0530

    Added class button at main dashboard

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -34,28 +34,81 @@ class _DashboardState extends State<Dashboard> {
         elevation: 0.0,
         actions: [],
       ),
-      body: FutureBuilder(
-        builder: (ctx, dataSnapshot) {
-          if (dataSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else {
-            if (dataSnapshot.error != null) {
-              //...error handling
-              return Center(
-                child: Text('Error Occurred!'),
-              );
-            } else {
-              return Consumer<Subjects>(
-                builder: (ctx, subjects, child) => ListView.builder(
-                  itemCount: subjects.subjectList.length,
-                  itemBuilder: (ctx, i) => SubjectCard(subjects.subjectList[i]),
-                ),
-              );
-            }
-          }
-        },
-        future: _subjectFuture,
-      ),
+      body: new Builder(builder: (BuildContext context) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 2.2,
+                    child: FlatButton(
+                      child: Text(
+                        "Class XI",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      onPressed: () {},
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 2.2,
+                    child: FlatButton(
+                      child: Text(
+                        "Class XII",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      onPressed: () {},
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder(
+                builder: (ctx, dataSnapshot) {
+                  if (dataSnapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else {
+                    if (dataSnapshot.error != null) {
+                      //...error handling
+                      return Center(
+                        child: Text('Error Occurred!'),
+                      );
+                    } else {
+                      return Consumer<Subjects>(
+                        builder: (ctx, subjects, child) => ListView.builder(
+                          itemCount: subjects.subjectList.length,
+                          itemBuilder: (ctx, i) =>
+                              SubjectCard(subjects.subjectList[i]),
+                        ),
+                      );
+                    }
+                  }
+                },
+                future: _subjectFuture,
+              ),
+            ),
+          ],
+        );
+      }),
       drawer: AppDrawer(),
     );
   }
